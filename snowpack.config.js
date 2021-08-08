a8dd65f6ff8aeb56767c1e59559242329128a358
savegnago <savegdesigner@gmail.com>
2021-08-08 18:59:12 -0300

    feat: add typescript

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   mount: {
     src: '/dist',
-    public: '/'
+    public: { url: '/', static: true }
   },
-  plugins: ['@snowpack/plugin-sass'],
+  plugins: ['@snowpack/plugin-sass', '@snowpack/plugin-typescript'],
   packageOptions: {
     /* ... */
   },
