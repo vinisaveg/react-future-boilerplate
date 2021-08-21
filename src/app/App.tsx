2db0088055d37d179900b350fe301a1a2d6f7df7
savegnago <savegdesigner@gmail.com>
2021-08-21 15:03:19 -0300

    feat: improve boilerplate App screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
 import React, { FunctionComponent } from 'react'
 
+import Logo from '../components/Logo'
 import './app.scss'
 
 const App: FunctionComponent = () => {
-  return <div className="app">Hello world!</div>
+  return (
+    <div className="app">
+      <Logo />
+      <h1>Reactjs Future Boilerplate</h1>
+    </div>
+  )
 }
 
 export default App
