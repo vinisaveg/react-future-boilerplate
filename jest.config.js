0667a02baeaa17315c3e96d3ddd8351fd5f4d4e0
savegnago <viniisaveeg@gmail.com>
2024-03-12 12:36:02 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,14 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '<rootDir>/test/**/*.{ts,tsx}', '!**/*.d.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/cypress'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom', 
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleNameMapper: {
     '\\.(css|scss|less)$': 'identity-obj-proxy'
-  }
+  },
+  "setupFilesAfterEnv": [
+    "<rootDir>/test/config/setupTests.ts"
+  ]
 }
